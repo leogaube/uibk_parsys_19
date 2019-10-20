68b54ca38778e75fff96525b2d054780aac62905
Leo <gaubeleo@gmail.com>
2019-10-20 16:43:19 +0200

    fix

--- proseminar/02/heat_stencil_1D/heat_stencil_1D_mpi.c
+++ proseminar/02/heat_stencil_1D/heat_stencil_1D_mpi.c
@@ -92,19 +92,27 @@ int main(int argc, char **argv) {
   int leftCell;
   int rightCell;
 
-  if (rank == 0)
-    printf("Hooloooo");
+  if (rank == 0){
+    printf("Hooloooo\n");
+    MPI_Send(&A[M-1], 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
+  }
+  if (rank == 1){
+    MPI_Recv(&leftCell, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    printf("%d\n", leftCell);
+  }
 
+/*
   if (rank != 0)
   {
     MPI_Recv(&leftCell, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    MPI_Send(A[0], 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD);
+    MPI_Send(&(A[0]), 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD);
   }
   if (rank != numProcs - 1)
   {
-    MPI_Send(A[M - 1], 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
+    MPI_Send(&(A[M - 1]), 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
     MPI_Recv(&rightCell, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
   }
+  */
 
   if (rank == 0)
     printf("Hallooo");
