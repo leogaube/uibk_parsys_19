1eeeeaa3eb6064241aa135c474151e59c0eac11b
LV 703651 - PS Parallele Systeme <cb761026@login.intern.lcc2.uibk.ac.at>
2019-10-20 09:15:04 +0200

    fixed the problem

--- proseminar/02/monte_carlo_method/pi_mpi.c
+++ proseminar/02/monte_carlo_method/pi_mpi.c
@@ -6,16 +6,18 @@
 #include <mpi.h>
 
 int main (int argc, char** argv) {
+	double start = MPI_Wtime();
+
 	MPI_Init(&argc, &argv);
-	// per default we create 100 million points
 	int size, rank;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	// If N is not a multiple of N than we just dont care because this is negliable in this scenario.
-	unsigned long N = 100000000 / size;
+	unsigned long N = 100000000;
 	if (argc > 1) {
 		sscanf(argv[1], "%lu", &N);
 	}
+	N /= size;
 	double x, y, z, pi;
 	int i = 0;
 	unsigned long count = 0;
@@ -30,12 +32,13 @@ int main (int argc, char** argv) {
 	}
 	
 	unsigned long pointsInside;
-	MPI_Reduce(&count, &pointsInside, 1, MPI_UNSINGED_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-
+	MPI_Reduce(&count, &pointsInside, 1, MPI_UNSIGNED_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
 
 	if (rank == 0) {
 		pi = (double) pointsInside / (N * size) * 4;
 		printf("number of trials = %lu, estimate of pi is %g \n", N * size, pi);
+		double end = MPI_Wtime();
+		printf("The process took %g seconds to finish. \n", end - start);
 	}
 
 	MPI_Finalize();
