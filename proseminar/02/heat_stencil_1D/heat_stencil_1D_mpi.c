05d4ac913f612a94a2723ee9bf57c373a5f2d814
Leo <gaubeleo@gmail.com>
2019-10-20 12:48:48 +0200

    fix

--- proseminar/02/heat_stencil_1D/heat_stencil_1D_mpi.c
+++ proseminar/02/heat_stencil_1D/heat_stencil_1D_mpi.c
@@ -30,10 +30,10 @@ int main(int argc, char **argv) {
   printf("Computing heat-distribution for room size N=%d for T=%d timesteps\n", N, T);
 
   // MPI setup
-  int myrank, numProcs;
+  int rank, numProcs;
 
   MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
 
   if (N % numProcs != 0){
@@ -41,39 +41,38 @@ int main(int argc, char **argv) {
     MPI_Finalize();
     return EXIT_FAILURE;
   }
-  int elemsPerRank = N / numProcs;
+  int M = N / numProcs;
 
   // ---------- setup ----------
 
   // create a buffer for storing temperature fields
 
-  Vector A;
+  Vector AA;
+  Vector A = createVector(M);
   int source_x;
-  if (myrank == 0)
+  if (rank == 0)
   {
-    A = createVector(N);
+    AA = createVector(N);
     // set up initial conditions in A
     for (int i = 0; i < N; i++) {
-      A[i] = 273; // temperature is 0° C everywhere (273 K)
+      AA[i] = 273; // temperature is 0° C everywhere (273 K)
     }
 
     // and there is a heat source in one corner
     source_x = N / 4;
-    A[source_x] = 273 + 60;
+    AA[source_x] = 273 + 60;
 
     printf("Initial:\t");
-    printTemperature(A, N);
+    printTemperature(AA, N);
     printf("\n");
   }
-  MPI_Bcast(&A, N, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Scatter(AA, M, MPI_INT, &A, M, MPI_INT, 0, MPI_COMM_WORLD);
   MPI_Bcast(&source_x, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  //value_t?
-  //MPI_Scatter(A, elemsPerRank, MPI_INT, &A_, elemsPerRank, MPI_INT, 0, MPI_COMM_WORLD);
 
   // ---------- compute ----------
 
   // create a second buffer for the computation
-  Vector B = createVector(N);
+  Vector B = createVector(M);
 
   // send/receive requests for left/right rank
   MPI_Request LSrequest;
@@ -81,17 +80,30 @@ int main(int argc, char **argv) {
   MPI_Request RSrequest;
   MPI_Request RRrequest;
 
+  int leftCell;
+  int rightCell;
+
+  if (rank != 0)
+  {
+    MPI_Recv(&leftCell, 1, MPI_INT, rank - 1, 42, MPI_COMM_WORLD, &LRrequest);
+    MPI_Send(&A[0], 1, MPI_INT, rank - 1, 41, MPI_COMM_WORLD, &LSrequest);
+  }
+  if (rank != numProcs - 1)
+  {
+    MPI_Send(&A[M-1], 1, MPI_INT, rank + 1, 42, MPI_COMM_WORLD, &RSrequest);
+    MPI_Recv(&rightCell, 1, MPI_INT, rank + 1, 41, MPI_COMM_WORLD, &RRrequest);
+  }
+
   // for each time step ..
   for (int t = 0; t < T; t++) {
     // .. we propagate the temperature
-    for (long long j = 0; j < elemsPerRank; j++)
+    for (long long i = 0; i < M; j++)
     {
-      int i = j + myrank * elemsPerRank;
-
       // center stays constant (the heat is still on)
-      if (i == source_x)
+      if (i + (rank * M) == source_x)
       {
         B[i] = A[i];
+        //TODO send receive
         continue;
       }
 
@@ -99,34 +111,46 @@ int main(int argc, char **argv) {
       value_t tc = A[i];
 
       // get temperatures of adjacent cells
-      value_t tl = (i != 0) ? A[i - 1] : tc;
-      value_t tr = (i != N - 1) ? A[i + 1] : tc;
+      value_t tl = (i != 0) ? A[i - 1] : ((rank != 0) ? leftCell : tc);
+      value_t tr = (i != M - 1) ? A[i + 1] : ((rank != numProcs-1) ? rightCell : tc);
 
       // compute new temperature at current position
       B[i] = tc + 0.2 * (tl + tr + (-2 * tc));
 
-      // send "data corners" of current rank to the prev and next rank respectively
-      if (j == 0 && myrank != 0){
-        MPI_Isend(&B[i], 1, MPI_INT, myrank - 1, 41, MPI_COMM_WORLD, &LSrequest);
-        MPI_Irecv(&B[i-1], 1, MPI_INT, myrank - 1, 42, MPI_COMM_WORLD, &LRrequest);
+      // send/receive "data corners" to/from the prev/next rank
+      if (i == 0 && rank != 0){
+        MPI_Isend(&B[i], 1, MPI_INT, rank - 1, 41, MPI_COMM_WORLD, &LSrequest);
+        MPI_Irecv(&leftCell, 1, MPI_INT, rank - 1, 42, MPI_COMM_WORLD, &LRrequest);
       }
-      else if (j == elemsPerRank-1 && myrank != numProcs-1){
-        MPI_Isend(&B[i], 1, MPI_INT, myrank + 1, 42, MPI_COMM_WORLD, &RSrequest);
-        MPI_Irecv(&B[i+1], 1, MPI_INT, myrank + 1, 41, MPI_COMM_WORLD, &RRrequest);
+      else if (i == M-1 && rank != numProcs-1){
+        MPI_Isend(&B[i], 1, MPI_INT, rank + 1, 42, MPI_COMM_WORLD, &RSrequest);
+        MPI_Irecv(&rightCell, 1, MPI_INT, rank + 1, 41, MPI_COMM_WORLD, &RRrequest);
       }
     }
 
     // swap matrices (just pointers, not content)
     MPI_Barrier(MPI_COMM_WORLD);
+    if (rank != 0)
+    {
+      MPI_Wait(&LSrequest, MPI_STATUS_IGNORE);
+      MPI_Wait(&LRrequest, MPI_STATUS_IGNORE);
+    }
+    if (rank != numProcs - 1)
+    {
+      MPI_Wait(&RSrequest, MPI_STATUS_IGNORE);
+      MPI_Wait(&RRrequest, MPI_STATUS_IGNORE);
+    }
 
     Vector H = A;
     A = B;
     B = H;
 
     // show intermediate step
-    if (rank == 0 && !(t % 1000)) {
+    if (rank == 0 && !(t % 1000))
+    {
+      MPI_Gather(&AA, M, MPI_INT, A, M, MPI_INT, 0, MPI_COMM_WORLD);
       printf("Step t=%d:\t", t);
-      printTemperature(A, N);
+      printTemperature(AA, N);
       printf("\n");
     }
   }
@@ -135,12 +159,15 @@ int main(int argc, char **argv) {
 
   // ---------- check ----------
 
-  printf("Final:\t\t");
-  printTemperature(A, N);
-  printf("\n");
+  if (rank == 0){
+    MPI_Gather(&A, M, MPI_INT, AA, M, MPI_INT, 0, MPI_COMM_WORLD);
+    printf("Final:\t\t");
+    printTemperature(A, N);
+    printf("\n");
+  }
 
   int success = 1;
-  for (long long i = 0; i < N; i++) {
+  for (long long i = 0; i < M; i++) {
     value_t temp = A[i];
     if (273 <= temp && temp <= 273 + 60)
       continue;
@@ -152,6 +179,10 @@ int main(int argc, char **argv) {
 
   // ---------- cleanup ----------
 
+  if (rank == 0)
+  {
+    releaseVector(AA);
+  }
   releaseVector(A);
 
   MPI_Finalize();
