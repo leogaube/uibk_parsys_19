fba112e3a367520c3887cda71b41ce4838a5e42a
Leo <gaubeleo@gmail.com>
2019-10-26 18:14:25 +0200

    ultimate makefile - working on lcc2

---
+++ proseminar/03/print_files.py
@@ -0,0 +1,23 @@
+import os, sys
+
+directory = sys.argv[1]
+
+if not os.path.isdir(directory):
+	print("'%s' does not exist!"%directory)
+	sys.exit(0)
+
+files = os.listdir(directory)
+if not files:
+	print("'%s' is empty!" % directory)
+	sys.exit(0)
+
+for filename in files:
+	full_filename = os.path.join(directory, filename)
+	if os.stat(full_filename).st_size == 0:
+		print("'%s' is empty!"%full_filename)
+	else:
+		with open(full_filename, "r") as f:
+			print("'%s':"%full_filename)
+			for line in f.readlines():
+				print("\t"+line)
+	print("")
