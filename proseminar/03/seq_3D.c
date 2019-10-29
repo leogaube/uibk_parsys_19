cf23164c5e64ba943e6d0eabb10328da4c299bee
Leo <gaubeleo@gmail.com>
2019-10-29 11:32:15 +0100

    my seq_3D implementation

--- proseminar/03/seq_3D.c
+++ proseminar/03/seq_3D.c
@@ -3,11 +3,32 @@
 
 #include <time.h>
 
-#include "heat_stencil.h"
+typedef float value_t;
 
-#define RESOLUTION 120
+#define RESOLUTION 25
 
-void printTemperature(Vector m, int nx, int ny, int nz);
+// -- vector utilities --
+
+typedef value_t *Vector;
+
+Vector createVector(int N);
+
+void releaseVector(Vector m);
+
+void printTemperature(Vector m, int Nx, int Ny, int Nz);
+
+int MAX(int x, int y){
+  return (x > y) ? x : y;
+}
+int MIN(int x, int y)
+{
+  return (x < y) ? x : y;
+}
+
+int IDX_3D(int x, int y, int z, int nx, int ny)
+{
+  return x + (y * nx) + (z * nx * ny);
+}
 
 // -- simulation code ---
 
@@ -15,91 +36,112 @@ int main(int argc, char **argv) {
   clock_t start = clock();
 
   // 'parsing' optional input parameter = problem size
-  int N = 10;
-  if (argc > 1) {
-    N = atoi(argv[1]);
-  }
-  int T = N;
-#ifdef VERBOSE
-  printf("Computing heat-distribution for room size N=%d for T=%d timesteps\n", N, T);
-#endif
+  int Nx = 100;
+  int Ny = 100;
+  int Nz = 100;
+
+  /*if (argc > 3) {
+    Nx = atoi(argv[1]);
+    Ny = atoi(argv[1]);
+    Nz = atoi(argv[1]);
+  }*/
+  int T = Nx * 500;
+  printf("Computing heat-distribution for room size N=%dx%dx%d for T=%d timesteps\n", Nx, Ny, Nz, T);
 
   // ---------- setup ----------
 
   // create a buffer for storing temperature fields
-  Vector A = createVector(N*N*N);
+  Vector A = createVector(Nx*Ny*Nz);
 
   // set up initial conditions in A
-  for (long i = 0; i < N*N*N; i++) {
+  for (int i = 0; i < Nx*Ny*Nz; i++) {
     A[i] = 273; // temperature is 0° C everywhere (273 K)
   }
 
   // and there is a heat source in one corner
-  int source_x = N / 4;
-  int source_y = source_x;
-  int source_z = source_x;
-  A[IDX_3D(source_x,source_y, source_z,N,N)] = 273 + 60;
+  int source_x = Nx / 4;
+  int source_y = Ny / 4;
+  int source_z = Nz / 4;
+  int source_i = IDX_3D(source_x, source_y, source_z, Nx, Ny);
+  A[source_i] = 273 + 60;
 
-#ifdef VERBOSE
   printf("Initial:\n");
-  printTemperature(A, N, N, N);
-#endif
+  printTemperature(A, Nx, Ny, Nz);
+  printf("\n");
+
   // ---------- compute ----------
 
   // create a second buffer for the computation
-  Vector B = createVector(N*N*N);
+  Vector B = createVector(Nx*Ny*Nz);
 
   // for each time step ..
   for (int t = 0; t < T; t++) {
-	// .. we propagate the temperature
-	for (int z = 0; z < N; z++) {
-		for (int y = 0; y < N; y++) {
-			for(int x = 0; x < N; x++){
-				// get the current idx
-				long i = IDX_3D(x,y,z,N,N);
+    // .. we propagate the temperature
+    for (int zi = 0; zi < Nz; zi++)
+    {
+      for (int yi = 0; yi < Ny; yi++)
+      {
+        for (int xi = 0; xi < Nx; xi++)
+        {
+          int i = IDX_3D(xi, yi, zi, Nx, Ny);
+          // center stays constant (the heat is still on)
+          if (source_i == i)
+          {
+            B[i] = A[i];
+            continue;
+          }
 
-				// center stays constant (the heat is still on)
-				if (i == IDX_3D(source_x,source_y,source_z,N,N)) {
-					B[i] = A[i];
-					continue;
-				}
+          // get temperature at current position
+          value_t tc = A[i];
 
-				// get temperature at current position
-				value_t tc = A[i];
+          // get temperatures of adjacent cells
+          value_t tl = (xi != 0) ? A[i-1] : tc;
+          value_t tr = (xi != Nx - 1) ? A[i+1] : tc;
 
-				// get temperatures of adjacent cells
-				value_t tl = (x != 0) ? A[IDX_3D(x-1,y,z,N,N)] : tc;
-				value_t tr = (x != N - 1) ? A[IDX_3D(x+1,y,z,N,N)] : tc;
-				value_t tu = (y != 0) ? A[IDX_3D(x,y-1,z,N,N)] : tc;
-				value_t td = (y != N - 1) ? A[IDX_3D(x,y+1,z,N,N)] : tc;
-				value_t tf = (z != 0) ? A[IDX_3D(x,y,z-1,N,N)] : tc;
-				value_t tb = (z != N - 1) ? A[IDX_3D(x,y,z+1,N,N)] : tc;
+          value_t tu = (yi != 0) ? A[i-Nx] : tc;
+          value_t td = (yi != Ny - 1) ? A[i+Nx] : tc;
 
+          value_t tf = (zi != 0) ? A[i-(Nx*Ny)] : tc;
+          value_t tb = (zi != Nz - 1) ? A[i+(Nx * Ny)] : tc;
 
-				// compute new temperature at current position
-				B[i] = tc + 0.2 * (tl + tr + tu + td + tf + tb + (-6 * tc));
-			}
-		}
-	}
-#ifdef VERBOSE
-  printf("t=%i:\n",t);
-  printTemperature(B, N, N, N);
-#endif
-	// swap matrices (just pointers, not content)
-	Vector H = A;
-	A = B;
-	B = H;
+          // compute new temperature at current position
+          B[i] = tc + 0.1666 * (tl + tr + tu + td + tf + tb + (-6 * tc));
+        }
+      }
+    }
+
+    // swap matrices (just pointers, not content)
+    Vector H = A;
+    A = B;
+    B = H;
+
+    // show intermediate step
+    if (!(t % 1000))
+    {
+      printf("Step t=%d:\n", t);
+      printTemperature(A, Nx, Ny, Nz);
+      printf("\n");
+    }
   }
 
   releaseVector(B);
-#ifdef VERBOSE
-  printf("Final:\n");
-  printTemperature(A, N, N, N);
-#endif
 
   // ---------- check ----------
-  double residual = is_verified_2D(A, N, N, source_x, source_y, T);
-  printf("The maximal deviation from the 1D theory is %fK.", residual);
+
+  printf("Final:\n");
+  printTemperature(A, Nx, Ny, Nz);
+  printf("\n");
+
+  int success = 1;
+  for (int i = 0; i < Nx*Ny; i++) {
+    value_t temp = A[i];
+    if (273 <= temp && temp <= 273 + 60)
+      continue;
+    success = 0;
+    break;
+  }
+
+  printf("Verification: %s\n", (success) ? "OK" : "FAILED");
 
   // ---------- cleanup ----------
 
@@ -109,10 +151,17 @@ int main(int argc, char **argv) {
   printf("The process took %g seconds to finish. \n", ((double)(end - start)) / CLOCKS_PER_SEC);
 
   // done
-  return EXIT_SUCCESS;
+  return (success) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
-void printTemperature(Vector m, int nx, int ny, int nz) {
+Vector createVector(int N) {
+  // create data and index vector
+  return malloc(sizeof(value_t) * N);
+}
+
+void releaseVector(Vector m) { free(m); }
+
+void printTemperature(Vector m, int Nx, int Ny, int Nz) {
   const char *colors = " .-:=+*^X#%@";
   const int numColors = 12;
 
@@ -121,45 +170,51 @@ void printTemperature(Vector m, int nx, int ny, int nz) {
   const value_t min = 273 + 0;
 
   // set the 'render' resolution
-  int W = RESOLUTION;
-  if(W>nx || W>ny || W>nz){
-	  W=MIN(nx,ny);
-	  W=MIN(W,nz);
-  }
+  int Wx = MIN(RESOLUTION, Nx);
+  int Wy = MIN(RESOLUTION, Ny);
+  int Wz = MIN(RESOLUTION, Nz);
 
   // step size in each dimension
-  int sWx = nx / W;
-  int sWy = ny / W;
-  int sWz = nz / W;
+  int sWx = Nx / Wx;
+  int sWy = Ny / Wy;
+  int sWz = Nz / Wz;
 
   // room
   // actual room
-  for(int k=0; k<W; k++){
-	  for(int j=0; j<W; j++){
-		  // left wall
-		  printf("X");
-		  for (int i = 0; i < W; i++) {
-			// get max temperature in this tile
-			value_t max_t = 0;
-			for (int z = sWz * k; z < sWz * k + sWz; z++) {
-				for (int y = sWy * j; y < sWy * j + sWy; y++) {
-					for (int x = sWx * i; x < sWx * i + sWx; x++) {
-						max_t = (max_t < m[IDX_3D(x,y,z,nx,ny)]) ? m[IDX_3D(x,y,z,nx,ny)] : max_t;
-					}
-				}
-			}
-			value_t temp = max_t;
+  for (int zi = 0; zi < Wz; zi++)
+  {
+    if (Wz != 1)
+      printf("########### SLICE %d ##############\n", zi);
+    for (int yi = 0; yi < Wy; yi++)
+    {
+      // left wall
+      printf("X");
+      for (int xi = 0; xi < Wx; xi++)
+      {
+        // get max temperature in this tile
+        value_t max_t = 0;
+        for (int z = sWz * zi; z < sWz * zi + sWz; z++)
+        {
+          for (int y = sWy * yi; y < sWy * yi + sWy; y++)
+          {
+            for (int x = sWx * xi; x < sWx * xi + sWx; x++)
+            {
+              max_t = (max_t < m[IDX_3D(x, y, z, Nx, Ny)]) ? m[IDX_3D(x, y, z, Nx, Ny)] : max_t;
+            }
+          }
+        }
+        value_t temp = max_t;
 
-			// pick the 'color'
-			int c = ((temp - min) / (max - min)) * numColors;
-			c = (c >= numColors) ? numColors - 1 : ((c < 0) ? 0 : c);
+        // pick the 'color'
+        int c = ((temp - min) / (max - min)) * numColors;
+        c = (c >= numColors) ? numColors - 1 : ((c < 0) ? 0 : c);
 
-			// print the average temperature
-			printf("%c ", colors[c]);
-		  }
-		  // right wall
-		  printf("X\n");
-	  }
-	  printf("\n");
+        // print the average temperature
+        printf("%c", colors[c]);
+      }
+      // right wall
+      printf("X\n");
+    }
   }
+  printf("\n##################################\n");
 }
