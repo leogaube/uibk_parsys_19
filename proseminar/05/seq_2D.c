be471b693325ca415095f1648a2d34160842456b
Mottl <mathias.meinschad>
2019-11-08 15:42:48 +0100

    added initialization and printing of particles
    the printing of particles is in relation to a specified room, however the printing function is not optimized and pretty stupidly implemented for now.

--- proseminar/05/seq_2D.c
+++ proseminar/05/seq_2D.c
@@ -4,23 +4,39 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <math.h>
 
 #include "gravity.h"
 
 int get_forces(double *forces_x, double *forces_y, Particle_p particles, int N);
 int apply_forces(double *forces_x, double *forces_y, Particle_p particles, int N);
+int init_particles(Particle_p particles, int N);
+int print_particles(Particle_p Particles, int N, int room_size);
 
 
 int main(int argc, char **argv)
 {
 	int N = 10;
+	int room_size = 20;
 	if (argc == 2){
 	    N = atoi(argv[1]);
+	}
+	if (argc == 3) {
+		N = atoi(argv[1]);
+		room_size = atoi(argv[2]);
 	}
 	int T = N*10;
 
 	// TODO init particles with random values
 	Particle_p particles = malloc(N*sizeof(Particle));
+	init_particles(particles, N);
+	
+	#ifdef VERBOSE
+	printf("Init:\n");
+	print_particles(particles, N, room_size);
+	#endif
 
 	// only upper triangular matrix (without diagonal) is needed
 	double *forces_x = malloc(((int)(N*(N-1)/2))*sizeof(double));
@@ -31,6 +47,12 @@ int main(int argc, char **argv)
 		apply_forces(forces_x, forces_y, particles, N);
 
 		//TODO plot results
+		#ifdef VERBOSE
+		if (t % 10 == 0) {
+			printf("time t: %d\n", t);
+			print_particles(particles, N, room_size);
+		}
+		#endif
 	}
 
 	// TODO verification
@@ -39,7 +61,6 @@ int main(int argc, char **argv)
 	free(forces_y);
 	free(particles);
 }
-
 
 /**
  * calculates the forces in x and y direction between the N particles
@@ -96,5 +117,61 @@ int apply_forces(double *forces_x, double *forces_y, Particle_p particles, int N
 	return EXIT_SUCCESS;
 }
 
+/**
+ * initializes the particles with random mass and random position
+ */
+int init_particles(Particle_p particles, int N) {
+	srand(time(NULL));
 
+	for (int i = 0; i < N; i++) {
+		// TODO what random values for mass should be generated?
+		particles[i].mass = rand() / (double) RAND_MAX * 10;
+		// exclusion of 1.0
+		particles[i].position.x = rand() / (double) ((unsigned)RAND_MAX + 1);
+		particles[i].position.y = rand() / (double) ((unsigned)RAND_MAX + 1);
+		particles[i].velocity.x = 0;
+		particles[i].velocity.y = 0;
+	}
 
+	return EXIT_SUCCESS;
+}
+
+/**
+ * function for checking of a particle is inside a specified index in relation to the room size
+ */
+bool index_contains_particle(Particle_p particles, int N, int i, int j, int room_size) {
+	for (int k = 0; k < N; k++) {
+		if ((int) (particles[k].position.x * room_size) == j && (int) (particles[k].position.y * room_size) == i) {
+			return true;
+		}
+	}
+	return false;
+}
+
+/**
+ * function for printing the room where the particles move
+ * the movement of the particles is in relation to the room size.
+ */
+int print_particles(Particle_p particles, int N, int room_size) {
+	char room_printed[room_size][room_size];
+
+	for (int i = 0; i < room_size; i++) {
+		for (int j = 0; j < room_size; j++) {
+			if (index_contains_particle(particles, N, i, j, room_size)) {
+				room_printed[i][j] = 'O';
+			} else {
+				room_printed[i][j] = ' ';
+			}
+		}
+	}
+
+	for (int i = 0; i < room_size; i++) {
+		printf("X");
+		for (int j = 0; j < room_size; j++) {
+			printf("%c", room_printed[i][j]);
+		}
+		printf("X\n");
+	}
+
+	return EXIT_SUCCESS;
+}
